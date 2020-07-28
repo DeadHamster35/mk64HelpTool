321a2b6c50c75b6ff8480820c397066aaeaf9008
DeadHamster35 <DeadHamstar@gmail.com>
2020-07-27 23:13:22 -0400

    Initial Upload

---
+++ CopyMan/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace MK64Help
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(12, 12);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(100, 20);
+            this.searchBox.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "MIO0";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Seg7";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(121, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Seg7";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(121, 104);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "MIO0";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Compress";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(121, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Expand";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(223, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(135, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Batch .C Texture->PNG";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(223, 41);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(135, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Inflate Vert Cache";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(223, 99);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(135, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Level Geometry -> TXT";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(223, 70);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(135, 23);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "Segment 4 -> TXT";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 174);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(346, 107);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 293);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MK64Help";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ CopyMan/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tarmac64;
+using Tarmac64_Geometry;
+using Tarmac64_Library;
+using Cereal64;
+using Texture64;
+using System.Drawing.Imaging;
+
+namespace MK64Help
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        TM64_Geometry tarmacLibrary = new TM64_Geometry();
+        FolderBrowserDialog folderOpen = new FolderBrowserDialog();
+        OpenFileDialog fileOpen = new OpenFileDialog();
+        SaveFileDialog fileSave = new SaveFileDialog();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string searchString = searchBox.Text;
+            
+
+            if (folderOpen.ShowDialog() == DialogResult.OK)
+            {
+                string input = folderOpen.SelectedPath;
+                if (folderOpen.ShowDialog() == DialogResult.OK)
+                {
+                    string output = folderOpen.SelectedPath;
+                    string[] files = Directory.GetFiles(input, searchString, SearchOption.AllDirectories);
+
+                    foreach (var copy in files)
+                    {
+                        File.Copy(copy, Path.Combine(output, Path.GetFileName(copy)));
+                    }
+                }
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (fileOpen.ShowDialog() == DialogResult.OK)
+            {
+                byte[] fileData = File.ReadAllBytes(fileOpen.FileName);
+
+
+
+                byte[] compressedData = tarmacLibrary.compressMIO0(fileData);
+                
+                if (fileSave.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllBytes(fileSave.FileName, compressedData);
+                }
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (fileOpen.ShowDialog() == DialogResult.OK)
+            {
+                byte[] fileData = File.ReadAllBytes(fileOpen.FileName);
+
+
+
+                byte[] uncompressedData = tarmacLibrary.decompressMIO0(fileData);
+
+                if (fileSave.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllBytes(fileSave.FileName, uncompressedData);
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (fileOpen.ShowDialog() == DialogResult.OK)
+            {
+                byte[] fileData = File.ReadAllBytes(fileOpen.FileName);
+
+
+
+                byte[] compressedData = tarmacLibrary.compress_seg7(fileData);
+
+                if (fileSave.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllBytes(fileSave.FileName, compressedData);
+                }
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (fileOpen.ShowDialog() == DialogResult.OK)
+            {
+                byte[] fileData = File.ReadAllBytes(fileOpen.FileName);
+
+
+
+                byte[] uncompressedData = tarmacLibrary.decompress_seg7(fileData);
+
+                if (fileSave.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllBytes(fileSave.FileName, uncompressedData);
+                }
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderOpen = new FolderBrowserDialog();
+            if (folderOpen.ShowDialog() == DialogResult.OK)
+            {
+
+                foreach (string file in Directory.GetFiles(folderOpen.SelectedPath, "*.c"))
+                {
+                    string[] fileData = File.ReadAllLines(file);
+                    List<byte> byteList = new List<byte>();
+
+                    //past the intro start at 1
+                    for (int currentLine = 1; currentLine < fileData.Length - 2; currentLine++)
+                    {
+                        string thisLine = fileData[currentLine];
+
+                        string[] itemArray = thisLine.Split(',');
+
+                        foreach (var currentItem in itemArray)
+                        {
+                            if (currentItem.Length > 0)
+                            {
+                                string editedItem = currentItem.Replace("0x", "");
+                                editedItem = editedItem.Replace("\"", "");
+                                byteList.Add(Convert.ToByte(editedItem, 16));
+                            }
+                        }
+                    }
+
+
+
+                    string outPath = Path.Combine(Path.GetDirectoryName(file), "Output");
+
+
+
+
+
+                    int addressAlign = 4 - (byteList.Count % 4);
+                    if (addressAlign == 4)
+                        addressAlign = 0;
+                    for (int align = 0; align < addressAlign; align++)
+                    {
+                        byteList.Add(0xFF);
+                    }
+
+                    byte[] byteArray = byteList.ToArray();
+                    Directory.CreateDirectory(outPath);
+                    outPath = Path.Combine(outPath, Path.GetFileName(file));
+                    File.WriteAllBytes(outPath + ".data.bin", byteArray);
+                    TM64_Geometry mk = new TM64_Geometry();
+
+                    try
+                    {
+                        byte[] textureData = mk.decompressMIO0(byteArray);
+
+
+                        int width, height = 0;
+                        byte[] voidBytes = new byte[0];
+
+                        if (textureData.Length == 0x800)
+                        {
+                            width = 32;
+                            height = 32;
+
+                            Bitmap exportBitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                            Graphics graphicsBitmap = Graphics.FromImage(exportBitmap);
+                            N64Graphics.RenderTexture(graphicsBitmap, textureData, voidBytes, 0, width, height, 1, N64Codec.RGBA16, N64IMode.AlphaCopyIntensity);
+
+                            string texturePath = (outPath + ".png");
+
+                            exportBitmap.Save(texturePath, ImageFormat.Png);
+
+
+                        }
+                        else
+                        {
+                            width = 32;
+                            height = 64;
+
+                            Bitmap exportBitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                            Graphics graphicsBitmap = Graphics.FromImage(exportBitmap);
+                            N64Graphics.RenderTexture(graphicsBitmap, textureData, voidBytes, 0, width, height, 1, N64Codec.RGBA16, N64IMode.AlphaCopyIntensity);
+
+                            string texturePath = (outPath + ".32x64.png");
+
+                            exportBitmap.Save(texturePath, ImageFormat.Png);
+
+                            width = 64;
+                            height = 32;
+
+                            exportBitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                            graphicsBitmap = Graphics.FromImage(exportBitmap);
+                            N64Graphics.RenderTexture(graphicsBitmap, textureData, voidBytes, 0, width, height, 1, N64Codec.RGBA16, N64IMode.AlphaCopyIntensity);
+
+                            texturePath = (outPath + ".64x32.png");
+
+                            exportBitmap.Save(texturePath, ImageFormat.Png);
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        continue;
+                    }
+
+
+
+                }
+
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileOpen = new OpenFileDialog();
+
+            if (fileOpen.ShowDialog() == DialogResult.OK)
+            {
+                byte[] segment4 = File.ReadAllBytes(fileOpen.FileName);
+
+
+
+                List<byte> insert = new List<byte> { 0x00, 0x00 };
+                int vertcount = segment4.Length / 14;
+
+                List<byte> vertList = segment4.ToList();
+                for (int i = 0; i < vertcount; i++)
+                {
+                    vertList.InsertRange((i + 1) * 10 + (i * 2) + (i * 4), insert);
+                }
+                byte[] seg4 = vertList.ToArray();
+
+                SaveFileDialog fileSave = new SaveFileDialog();
+                if (fileSave.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllBytes(fileSave.FileName, seg4);
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Segment 4 Uncompressed");
+            OpenFileDialog fileOpen = new OpenFileDialog();
+
+            if (fileOpen.ShowDialog() == DialogResult.OK)
+            {
+                byte[] segment4 = File.ReadAllBytes(fileOpen.FileName);
+
+                MessageBox.Show("Segment 7 Uncompressed");
+
+                if (fileOpen.ShowDialog() == DialogResult.OK)
+                {
+                    byte[] segment7 = File.ReadAllBytes(fileOpen.FileName);
+
+                    MessageBox.Show("File Save");
+                    SaveFileDialog fileSave = new SaveFileDialog();
+
+                    if (fileSave.ShowDialog() == DialogResult.OK)
+                    {
+                        tarmacLibrary.dumpface2(segment4, segment7, fileSave.FileName);
+                    }
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                byte[] vertData = File.ReadAllBytes(openFile.FileName);
+
+                string[] outData = tarmacLibrary.DumpVerts(vertData);
+
+                SaveFileDialog saveFile = new SaveFileDialog();
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllLines(saveFile.FileName, outData);
+                }
+
+            }
+        }
+    }
+    
+    
+}
